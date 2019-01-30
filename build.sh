ee9fd023510e0db55cb328a261077ad811b2a053
filwie <filip.wiechec@gmail.com>
2019-01-30 17:41:12 +0100

    Refactor build script to allow picking images to build

--- build.sh
+++ build.sh
@@ -1,42 +1,87 @@
 #!/usr/bin/env zsh
-set -e
+set -eu
 
-REPO_DIR="${0:a:h}"  # where the script resides
-IMAGE_PREFIX="custom-"
-IMAGE_TAG="stable"
+REPO_DIR="${0:h}"  # where the script resides
+pushd "${REPO_DIR}"
 
-function handle_args () {
-    return
+IMAGE_PREFIX="${IMAGE_PREFIX:-custom-}"
+IMAGE_TAG="${IMAGE_TAG:=latest}"
+
+VERBOSE_SWITCH="--quiet"
+declare -au IMAGES_TO_BUILD
+DOCKERFILES=(**/Dockerfile)
+
+function usage () {
+    echo -e"USAGE:\n\t${0} [-v/--verbose] [image] {[image]}"
+    exit 1
 }
 
-function quiet () {
-    eval "${1} > /dev/null"
+function handle_args () {
+    while (( ${#} )); do
+        case "${1}" in
+            -h|--help)
+                usage
+                ;;
+            -v|--verbose)
+                VERBOSE_SWITCH=""
+                ;;
+            -s|--silent)
+                VERBOSE_SWITCH="-s"
+                ;;
+            *)
+                [[ -f "${1}/Dockerfile" ]] && IMAGES_TO_BUILD+=("${1}")
+                ;;
+        esac
+        shift
+    done
+
+    if [[ ${#IMAGES_TO_BUILD[@]} -eq 0 ]]; then
+        for dockerfile in "${DOCKERFILES[@]}"; do
+            IMAGES_TO_BUILD+=("${dockerfile:h}")
+        done
+    fi
+}
+
+function _quiet () { echo -n "${1} > /dev/null" }
+function _silent () { echo -n "${1} &> /dev/null" }
+
+function _run_cmd () {
+    [[ -n "${1}" ]] || return
+    case "${2}" in
+        -s|--silent)
+            eval "$(_silent ${1})" ;;
+        -q|--quiet)
+            eval "$(_quiet ${1})" ;;
+        *)
+            eval "${1}" ;;
+    esac
+}
+
+function run_log_cmd () {
+    [[ -n "${1}" ]] || return
+    echo -e "$(tput setaf 12)[$(date +'%H:%M:%S')] RUNNING: ${1}$(tput sgr0)"
+    _run_cmd ${1} "${2:-${VERBOSE_SWITCH}}"
 }
 
 function _build () {
     local dockerfile_dir="${1}"
     local image_name="${IMAGE_PREFIX}${2-${dockerfile_dir}}"
     local image_tag="${3:-${IMAGE_TAG}}"
-    quiet "pushd ${dockerfile_dir}"
-    docker build . -t "${image_name}:${image_tag}"
-    quiet "popd"
+    run_log_cmd "pushd ${dockerfile_dir}"
+    run_log_cmd "docker build . -t ${image_name}:${image_tag}"
+    run_log_cmd "popd"
 }
 
 function build_images () {
-    local dockerfiles=(**/Dockerfile)
-    if [[ -n "${1}" ]] && [[ -f "${1}/Dockerfile" ]]; then
-        local dockerfiles=("${1}/Dockefile")
-    fi
-
-    for dockerfile in "${dockerfiles[@]}"; do
-        _build "$(dirname ${dockerfile})"
+    for image in "${IMAGES_TO_BUILD[@]}"; do
+        _build "${image}"
     done
 }
 
 function main () {
-    pushd "${REPO_DIR}"
-    build_images "${@}"
-    popd
+    handle_args "${@}"
+    build_images
 }
 
 main "${@}"
+popd
